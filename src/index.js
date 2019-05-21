ed77062b5df2326e4be1c3c99c0fda944b3c58bd
ShashankSharma17 <shashank.sharma1795@outlook.com>
2019-05-21 21:48:17 +0530

    Section 8 Completed

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import { Provider } from 'react-redux'
 import 'semantic-ui-css/semantic.min.css'
 import './index.css'
 import App from './app/layout/App'
-import registerServiceWorker from './registerServiceWorker'
+import * as serviceWorker from './serviceWorker'
 import { configureStore } from './app/store/configureStore'
+import ScrollToTop from './app/common/util/ScrollToTop'
 
 const store = configureStore()
 
@@ -16,7 +17,9 @@ let render = () => {
   ReactDOM.render(
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <ScrollToTop>
+          <App />
+        </ScrollToTop>
       </BrowserRouter>
     </Provider>,
     rootEl
@@ -31,4 +34,8 @@ if (module.hot) {
 
 render()
 
-registerServiceWorker()
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+
+serviceWorker.unregister()
