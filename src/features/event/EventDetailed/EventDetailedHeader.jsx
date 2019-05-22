78fc693e7252409a56b32fa4bc318012409e985d
ShashankSharma17 <shashank.sharma1795@outlook.com>
2019-05-22 12:46:52 +0530

    Section 13 End

--- src/features/event/EventDetailed/EventDetailedHeader.jsx
+++ src/features/event/EventDetailed/EventDetailedHeader.jsx
@@ -1,36 +1,41 @@
-import React from 'react';
-import { Segment, Image, Item, Header, Button } from 'semantic-ui-react';
+import React from 'react'
+import { Segment, Image, Item, Header, Button } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
+import format from 'date-fns/format'
 
 const eventImageStyle = {
-    filter: 'brightness(30%)'
-};
+  filter: 'brightness(30%)'
+}
 
 const eventImageTextStyle = {
-    position: 'absolute',
-    bottom: '5%',
-    left: '5%',
-    width: '100%',
-    height: 'auto',
-    color: 'white'
-};
+  position: 'absolute',
+  bottom: '5%',
+  left: '5%',
+  width: '100%',
+  height: 'auto',
+  color: 'white'
+}
 
-const EventDetailedHeader = ({event}) => {
+const EventDetailedHeader = ({ event }) => {
   return (
     <Segment.Group>
-      <Segment basic attached="top" style={{ padding: '0' }}>
-        <Image src={`/assets/categoryImages/${event.category}.jpg`} fluid style={eventImageStyle} />
+      <Segment basic attached='top' style={{ padding: '0' }}>
+        <Image
+          src={`/assets/categoryImages/${event.category}.jpg`}
+          fluid
+          style={eventImageStyle}
+        />
 
         <Segment basic style={eventImageTextStyle}>
-        <Item.Group>
+          <Item.Group>
             <Item>
               <Item.Content>
                 <Header
-                  size="huge"
+                  size='huge'
                   content={event.title}
                   style={{ color: 'white' }}
                 />
-                <p>{event.date}</p>
+                <p>{format(event.date, 'dddd Do MMM')}</p>
                 <p>
                   Hosted by <strong>{event.hostedBy}</strong>
                 </p>
@@ -40,16 +45,21 @@ const EventDetailedHeader = ({event}) => {
         </Segment>
       </Segment>
 
-      <Segment attached="bottom">
+      <Segment attached='bottom'>
         <Button>Cancel My Place</Button>
-        <Button color="teal">JOIN THIS EVENT</Button>
+        <Button color='teal'>JOIN THIS EVENT</Button>
 
-        <Button as={Link} to={`/manage/${event.id}`} color="orange" floated="right">
+        <Button
+          as={Link}
+          to={`/manage/${event.id}`}
+          color='orange'
+          floated='right'
+        >
           Manage Event
         </Button>
       </Segment>
     </Segment.Group>
-  );
-};
+  )
+}
 
-export default EventDetailedHeader;
+export default EventDetailedHeader
