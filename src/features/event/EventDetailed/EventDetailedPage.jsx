3ffd031e08cc58caeebed191d898a321b66bb656
ShashankSharma17 <shashank.sharma1795@outlook.com>
2019-05-23 20:48:40 +0530

    Section 20 End

--- src/features/event/EventDetailed/EventDetailedPage.jsx
+++ src/features/event/EventDetailed/EventDetailedPage.jsx
@@ -1,38 +1,57 @@
-import React from 'react';
-import { Grid } from 'semantic-ui-react';
+import React, { Component } from 'react'
+import { Grid } from 'semantic-ui-react'
 import { connect } from 'react-redux'
-import EventDetailedHeader from './EventDetailedHeader';
-import EventDetailedInfo from './EventDetailedInfo';
-import EventDetailedChat from './EventDetailedChat';
-import EventDetailedSidebar from './EventDetailedSidebar';
+import { firestoreConnect, isEmpty } from 'react-redux-firebase'
+import { compose } from 'redux'
+import UserDetailedHeader from './UserDetailedHeader'
+import UserDetailedDescription from './UserDetailedDescription'
+import UserDetailedPhotos from './UserDetailedPhotos'
+import UserDetailedSidebar from './UserDetailedSidebar'
+import UserDetailedEvents from './UserDetailedEvents'
+import { userDetailedQuery } from '../userQueries'
+import LoadingComponent from '../../../app/layout/LoadingComponent'
 
 const mapState = (state, ownProps) => {
-  const eventId = ownProps.match.params.id;
+  let userUid = null
+  let profile = {}
 
-  let event = {};
-
-  if (eventId && state.events.length > 0) {
-    event = state.events.filter(event => event.id === eventId)[0]
+  if (ownProps.match.params.id === state.auth.uid) {
+    profile = state.firebase.profile
+  } else {
+    profile =
+      !isEmpty(state.firestore.ordered.profile) &&
+      state.firestore.ordered.profile[0]
+    userUid = ownProps.match.params.id
   }
-
   return {
-    event
+    profile,
+    userUid,
+    auth: state.firebase.auth,
+    photos: state.firestore.ordered.photos,
+    requesting: state.firestore.status.requesting
   }
 }
 
-const EventDetailedPage = ({event}) => {
-  return (
-    <Grid>
-      <Grid.Column width={10}>
-        <EventDetailedHeader event={event} />
-        <EventDetailedInfo event={event} />
-        <EventDetailedChat />
-      </Grid.Column>
-      <Grid.Column width={6}>
-        <EventDetailedSidebar attendees={event.attendees}/>
-      </Grid.Column>
-    </Grid>
-  );
-};
+class UserDetailedPage extends Component {
+  render() {
+    const { profile, photos, auth, match, requesting } = this.props
+    const isCurrentUser = auth.uid === match.params.id
+    const loading = Object.values(requesting).some(a => a === true)
 
-export default connect(mapState)(EventDetailedPage);
+    if (loading) return <LoadingComponent inverted={true} />
+    return (
+      <Grid>
+        <UserDetailedHeader profile={profile} />
+        <UserDetailedDescription profile={profile} />
+        <UserDetailedSidebar isCurrentUser={isCurrentUser} />
+        {photos && photos.length > 0 && <UserDetailedPhotos photos={photos} />}
+        <UserDetailedEvents />
+      </Grid>
+    )
+  }
+}
+
+export default compose(
+  connect(mapState),
+  firestoreConnect((auth, userUid) => userDetailedQuery(auth, userUid))
+)(UserDetailedPage)
