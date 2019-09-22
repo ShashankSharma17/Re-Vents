8ea23644abbde4ad2641a4c53b096383bfe2f5ec
Shashank Sharma <shashank.sharma1795@outlook.com>
2019-09-22 21:49:24 +0530

    Update firebase.js

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/firestore'
 
 // Shashank web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBdycS-ev8PsirXNdpCs2ufID8KGX5ylgg',
-  authDomain: 're-vents-241317.firebaseapp.com',
-  databaseURL: 'https://re-vents-241317.firebaseio.com',
-  projectId: 're-vents-241317',
-  storageBucket: 're-vents-241317.appspot.com',
-  messagingSenderId: '602301689743',
-  appId: '1:602301689743:web:0ce047f8886ca7cb'
+  apiKey: '',    
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: ''
 }
 
 // Initialize Firebase
